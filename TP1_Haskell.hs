40015a58fefd1288530df0c7853acf418a658ae7
ntaboada <ntaboada93@gmail.com>
2015-04-15 16:27:23 -0300

    Create TP1_Haskell.hs
    Hecho el punto 3 (esMensajito)

---
+++ TP1_Haskell.hs
@@ -0,0 +1,16 @@
+
+Punto 3)
+
+lengthStrings lista_cadenas = map (length) lista_cadenas 
+
+filterSmallStrings lista_lengths = filter (<=3) lista_lengths
+
+filterBigStrings lista_lengths = filter (>3) lista_lengths
+
+mensajito lista_cadenas | length (filterSmallStrings (lengthStrings lista_cadenas)) > length (filterBigStrings (lengthStrings lista_cadenas))  = True
+						            |  length (filterSmallStrings (lengthStrings lista_cadenas)) < length (filterBigStrings (lengthStrings lista_cadenas))  = False
+						
+mensajitoOK lista_cadenas | length (filterSmallStrings (lengthStrings lista_cadenas)) > length (filterBigStrings (lengthStrings lista_cadenas))  = "Es mensajito"
+						              |  length (filterSmallStrings (lengthStrings lista_cadenas)) < length (filterBigStrings (lengthStrings lista_cadenas))  = "No es mensajito"
+						              | otherwise = "Misma cantidad"
+
