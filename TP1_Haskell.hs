28e2ea592a83ae097497aa6c990c9ff6276d55ca
ntaboada <ntaboada93@gmail.com>
2015-04-15 22:52:25 -0300

    Agrego puntos 1, y 2

--- TP1_Haskell.hs
+++ TP1_Haskell.hs
@@ -1,5 +1,36 @@
+Punto 1) Generar la cadena masVocalesQueConsonantes que recibe una cadena y  devuelve True o False
 
-Punto 3)
+esVocal caracter = elem caracter "aeiou"
+
+esConsonante caracter = not (esVocal caracter)
+
+filtrarConsonantes cadena = filter (esConsonante) cadena
+
+filtrarVocales cadena = filter (esVocal) cadena
+
+cuentaVocales cadena = length (filtrarVocales cadena)
+
+cuentaConsonantes cadena = length (filtrarConsonantes cadena)
+
+
+masVocalesQueConsonantes cadena = (cuentaVocales cadena) > (cuentaConsonantes cadena) 
+
+masVocalesQueConsonantes2  cadena 
+
+			| (cuentaVocales cadena) > (cuentaConsonantes cadena) = "Mas"
+	   		| (cuentaVocales cadena) < (cuentaConsonantes cadena) = "Menos"
+	   		| otherwise = "Igual"
+
+
+Punto 2) Desarrollar funcion esPrimo que recibe un numero y devuelve True o False
+
+listaNumerosAEvaluar x = [ x | x<-[2..x-1]]
+
+divisionPorNumero x = map (mod x) (listaNumerosAEvaluar x)
+
+esPrimo x = not (any (== 0) (divisionPorNumero x))
+
+Punto 3) Desarrollar la funcion mensajito que recibe un conjunto de cadenas (["hola", "q", "kc"]) y retorna True o False si la cantidad de cadenas con longitud menor a 3 es mayor a la cantidad de cadenas mayor a 3 de longitud
 
 lengthStrings lista_cadenas = map (length) lista_cadenas 
 
@@ -14,6 +45,7 @@ mensajitoOK lista_cadenas | length (filterSmallStrings (lengthStrings lista_cade
 						              |  length (filterSmallStrings (lengthStrings lista_cadenas)) < length (filterBigStrings (lengthStrings lista_cadenas))  = "No es mensajito"
 						              | otherwise = "Misma cantidad"
 
-Punto 4) 
+Punto 4) Desarrollar la funcion cuentaPalabrasRusasEnTexto. Una palabra es considerada rusa si tiene los caracteres "k" e "y" y no tiene mas de dos vocales
 
-Empezando a hacer el Punto 4
+
+
